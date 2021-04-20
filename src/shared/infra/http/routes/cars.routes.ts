01b7507d32dbe0ed4ef8e87df1e432591136502f
gabrielrom <gabrielpessoal626@gmail.com>
2021-04-19 23:33:23 -0300

    Add func of add images for a car

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,17 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '@configs/upload';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
+import { UploadImagesCarController } from '@modules/cars/useCases/uploadImagesCar/UploadImagesCarController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import ensureUserIsAdmin from '../middlewares/ensureUserIsAdmin';
 
 const carsRouter = Router();
 
+const upload = multer(uploadConfig.upload('./tmp/cars'));
+
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadImagesCarController = new UploadImagesCarController();
 
 carsRouter.post(
   '/',
@@ -27,6 +33,14 @@ carsRouter.post(
   ensureAuthenticated,
   ensureUserIsAdmin,
   createCarSpecificationController.handle,
+);
+
+carsRouter.post(
+  '/images/:id',
+  ensureAuthenticated,
+  ensureUserIsAdmin,
+  upload.array('images'),
+  uploadImagesCarController.handle,
 );
 
 export default carsRouter;
